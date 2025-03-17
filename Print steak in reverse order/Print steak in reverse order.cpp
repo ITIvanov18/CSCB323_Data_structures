0e51f068745c6535f0b6fd6342be75cfe5b60ce6
ITIvanov18 <ITIvanov18@codingburgas.bg>
2025-03-17 23:52:21 +0200

    Commit Print steak in reverse order task

---
+++ Print steak in reverse order/Print steak in reverse order.cpp
@@ -0,0 +1,66 @@
+// Print steak in reverse order.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <ctype.h>
+using namespace std;
+
+typedef struct kutiq* p;
+
+struct kutiq {
+    p next;
+    char letter;
+};
+
+void printStackNaopaki(p tuk) {
+    if (tuk->next != NULL) {
+        printStackNaopaki(tuk->next); // Рекурсивно извикване
+    } else {
+        cout << "Duno na steka" << endl; // Край на списъка
+    }
+    cout << tuk->letter;
+}
+
+int main() {
+    p na = NULL;
+    p pamet;
+    p help;
+    char neshto = 'a';
+
+    // Създаване на списък
+    while (isalpha(neshto) || neshto == ' ') {
+        pamet = new kutiq;
+        neshto = cin.get();
+        pamet->letter = neshto;
+        pamet->next = na;
+        na = pamet;
+    }
+
+    // Премахване на първия елемент
+    help = na;
+    na = na->next;
+    delete help;
+
+    p iterator = na;
+    while (iterator != NULL) {
+        cout << iterator->letter;
+        iterator = iterator->next;
+    }
+    cout << endl;
+
+    delete iterator;
+
+    return 0;
+}
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
